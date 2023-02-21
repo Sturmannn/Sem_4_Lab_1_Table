70ab42a945c753bafa98e623e71e72981af47831
unknown <itmm-2113-a@E.local>
2023-02-21 09:51:04 +0300

    Работа в аудитории

---
+++ TableLib/Polynom.h
@@ -0,0 +1,8 @@
+#pragma once
+
+int Add(int a = 0, int b = 0)
+{
+	int c;
+	c = a + b;
+	return c;
+}
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,6 +1,7 @@
-#include "TestLib.h"
+#include <gtest.h>
 
-int main()
+int main(int argc, char** argv)
 {
-  return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
